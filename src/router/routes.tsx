faa9afe701ab147cdad566a13e2d0ca1d5a8f9cc
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-09-26 15:02:05 +0900

    Feat: 회원가입 페이지 경로 추가

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -2,12 +2,14 @@ import { lazy } from 'react'
 import { Route, Routes as RouterRoutes } from 'react-router-dom'
 
 const MainPage = lazy(() => import('../pages/MainPage'))
+const SignupPage = lazy(() => import('../pages/SignupPage'))
 const ChatPage = lazy(() => import('../pages/ChatPage'))
 
 export function Routes() {
   return (
     <RouterRoutes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/signup" element={<SignupPage />} />
       <Route path="/chat" element={<ChatPage />} />
     </RouterRoutes>
   )
