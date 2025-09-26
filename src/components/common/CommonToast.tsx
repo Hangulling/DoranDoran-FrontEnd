f9d0ef0239b04c36bf82206ab86b0b5eefe078d3
somin0567 <koala0567@gmail.com>
2025-09-26 20:11:33 +0900

    Feat: 공통 토스트 추가

---
+++ src/components/common/CommonToast.tsx
@@ -0,0 +1,32 @@
+import { type JSX } from 'react'
+import { toast } from 'react-hot-toast'
+import ErrorIcon from '../../assets/icon/error.svg'
+import CheckIcon from '../../assets/icon/checkRound.svg'
+
+type IconType = 'error' | 'checkRound'
+
+// 에러 / 체크 아이콘 선택
+const iconMap: Record<IconType, JSX.Element> = {
+  error: <img src={ErrorIcon} alt="error" />,
+  checkRound: <img src={CheckIcon} alt="check" />,
+}
+
+type ToastProps = {
+  message: string
+  iconType?: IconType
+}
+
+const showToast = ({ message, iconType }: ToastProps) => {
+  toast.custom(
+    <div className="flex items-start w-full mx-[20px] mb-[20px] bg-[rgba(15,16,16,0.8)] px-[14px] py-[16px] rounded-[12px] gap-[8px]">
+      {iconType && iconMap[iconType]}
+      <span className="text-subtitle text-[14px] text-white">{message}</span>
+    </div>,
+    {
+      duration: 4000,
+      position: 'bottom-center',
+    }
+  )
+}
+
+export default showToast
