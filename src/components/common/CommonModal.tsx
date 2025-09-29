cf997b5e75b2a6fe72a0d06b54eabd19f355aa13
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-09-29 17:56:00 +0900

    Style: 회원가입 모달 UI 추가

--- src/components/common/CommonModal.tsx
+++ src/components/common/CommonModal.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import checkIcon from '../../assets/icon/signupCheck.svg'
+import Button from './Button'
 
 interface CommonModalProps {
   open: boolean
   title: string
+  variant?: 'common' | 'signup'
   description?: string | string[] // 상수 지정시 배열
   confirmText?: string
   cancelText?: string
@@ -13,6 +16,7 @@ interface CommonModalProps {
 const CommonModal: React.FC<CommonModalProps> = ({
   open,
   title,
+  variant = 'common',
   description,
   confirmText = '확인',
   cancelText = '닫기',
@@ -21,12 +25,18 @@ const CommonModal: React.FC<CommonModalProps> = ({
 }) => {
   if (!open) return null
 
+  const isSignup = variant === 'signup'
+
   return (
     <>
       <div className="modal modal-open" role="dialog">
         <div className="modal-box rounded-lg max-w-[303px] py-5 px-4 flex flex-col items-center">
+          {isSignup && (
+            <div className="m-2">
+              <img src={checkIcon} className="w-10 h-10" />
+            </div>
+          )}
           <h3 className="text-title text-[18px] mb-2">{title}</h3>
-
           {Array.isArray(description) ? (
             <div className="text-[14px] text-center">
               {description.map((line, idx) => (
@@ -37,20 +47,33 @@ const CommonModal: React.FC<CommonModalProps> = ({
             <p className="text-[14px] text-center">{description}</p>
           )}
 
-          <div className="modal-action w-full flex gap-x-[14px] mt-4">
-            <button
-              className="bg-gray-80 text-subtitle text-[14px] text-gray-600 rounded-lg flex-1 py-[14px]"
-              onClick={onCancel}
-            >
-              {cancelText}
-            </button>
-            <button
-              className="bg-green-400 text-subtitle text-[14px] text-white rounded-lg flex-1 py-[14px]"
-              onClick={onConfirm}
-            >
-              {confirmText}
-            </button>
-          </div>
+          {isSignup ? (
+            <div className="modal-action w-full flex mt-4">
+              <Button
+                variant="confirm"
+                onClick={onConfirm}
+                size="lg"
+                className="text-[14px] text-white py-[14px]"
+              >
+                {confirmText}
+              </Button>
+            </div>
+          ) : (
+            <div className="modal-action w-full flex gap-x-[14px] mt-4">
+              <button
+                className="bg-gray-80 text-subtitle text-[14px] text-gray-600 rounded-lg flex-1 py-[14px]"
+                onClick={onCancel}
+              >
+                {cancelText}
+              </button>
+              <button
+                className="bg-green-400 text-subtitle text-[14px] text-white rounded-lg flex-1 py-[14px]"
+                onClick={onConfirm}
+              >
+                {confirmText}
+              </button>
+            </div>
+          )}
         </div>
 
         <div className="modal-backdrop bg-black/65" onClick={onCancel}></div>
