0013cc46b117aea3b59c12f2687901d43160c886
somin0567 <koala0567@gmail.com>
2025-10-07 03:59:50 +0900

    Feat: 채팅창 친밀도 바 추가

---
+++ src/components/chat/ClosenessBar.tsx
@@ -0,0 +1,23 @@
+import { chatRooms } from '../../mocks/db/chat'
+import useClosenessStore from '../../stores/useClosenessStore'
+
+interface ClosenessBarProps {
+  chatRoomId: string
+}
+
+const ClosenessBar: React.FC<ClosenessBarProps> = ({ chatRoomId }) => {
+  const closeness = useClosenessStore(state => state.closenessMap[chatRoomId] ?? 1)
+
+  const room = chatRooms.find(room => room.id === Number(chatRoomId))
+  const chatRoomLabel = room ? room.title : `ChatRoom ${chatRoomId}`
+
+  const closenessText = closeness == 1 ? 'casual' : closeness == 2 ? 'friendly' : 'close'
+
+  return (
+    <div className="mx-auto w-full max-w-md left-1/2 translate-x-[-50%] h-[33px] bg-green-350 overflow-hidden relative flex items-center justify-center text-white text-[14px] select-none">
+      Chatting with a {closenessText} {chatRoomLabel.toLowerCase()}
+    </div>
+  )
+}
+
+export default ClosenessBar
--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -10,13 +10,17 @@ import Button from './Button'
 import { useState } from 'react'
 import Sidebar from './SideBar'
 import type { NavBarProps } from '../../types/common'
+import { useModalStore } from '../../stores/useUiStateStore'
+import ExitModal from '../chat/ExitModal'
 
 const NavBar: React.FC<NavBarProps> = ({ title, isMain, showBookmark, showDelete }) => {
   const navigate = useNavigate()
   const { selectionMode, deleteMode, enterSelectionMode, exitSelectionMode, selectAll, delectAll } =
     useArchiveStore()
+  const noShowAgain = useModalStore(state => state.noShowAgain)
 
   const [sidebarOpen, setSidebarOpen] = useState(false)
+  const [modalOpen, setModalOpen] = useState(false)
   const chatMatch = useMatch('/chat/:id')
   const closenessMatch = useMatch('/closeness/:id')
   const archiveMatch = useMatch('/archive/:id')
@@ -24,9 +28,28 @@ const NavBar: React.FC<NavBarProps> = ({ title, isMain, showBookmark, showDelete
 
   // 뒤로가기
   const goBack = () => {
-    navigate(-1)
+    if (chatMatch) {
+      if (noShowAgain) {
+        navigate('/')
+      } else {
+        setModalOpen(true)
+      }
+    } else {
+      navigate(-1)
+    }
   }
 
+  // 채팅창 뒤로가기 확인 모달
+  const handleConfirm = () => {
+    setModalOpen(false)
+    navigate('/')
+  }
+
+  const handleCancel = () => {
+    setModalOpen(false)
+  }
+
+  // 북마크 바로가기
   const handleBookmarkClick = () => {
     if (!currentId) {
       navigate('/archive/2')
@@ -41,73 +64,80 @@ const NavBar: React.FC<NavBarProps> = ({ title, isMain, showBookmark, showDelete
     }
   }
 
+  // 사이드바 토글
   const toggleSidebar = () => {
     setSidebarOpen(open => !open)
   }
 
   return (
-    <div className="fixed top-0 mx-auto w-full max-w-md left-1/2 translate-x-[-50%] navbar bg-white shadow-[0_1px_2px_rgba(0,0,0,0.12)] h-15 min-h-15 p-0 z-50">
-      <div className="navbar-start ml-5">
-        {/* 뒤로가기 */}
-        {!isMain && !selectionMode && (
-          <button onClick={goBack}>
-            <img src={LeftArrowIcon} alt="뒤로가기" />
-          </button>
-        )}
-        {selectionMode && (
-          <Button onClick={exitSelectionMode}>
-            <img src={CloseIcon} />
-          </Button>
-        )}
-        {/* 햄버거 */}
-        {isMain && !selectionMode && (
-          <button onClick={toggleSidebar}>
-            <Hamburger />
-          </button>
-        )}
+    <>
+      <div className="fixed top-0 mx-auto w-full max-w-md left-1/2 translate-x-[-50%] navbar bg-white shadow-[0_1px_2px_rgba(0,0,0,0.12)] h-15 min-h-15 p-0 z-50">
+        <div className="navbar-start ml-5">
+          {/* 뒤로가기 */}
+          {!isMain && !selectionMode && (
+            <button onClick={goBack}>
+              <img src={LeftArrowIcon} alt="뒤로가기" />
+            </button>
+          )}
+
+          {selectionMode && (
+            <Button onClick={exitSelectionMode}>
+              <img src={CloseIcon} />
+            </Button>
+          )}
+
+          {/* 햄버거 */}
+          {isMain && !selectionMode && (
+            <button onClick={toggleSidebar}>
+              <Hamburger />
+            </button>
+          )}
+        </div>
+
+        {/* 로고 및 페이지이름 */}
+        <div className="navbar-center">
+          {/* 임시 Link */}
+          {isMain ? (
+            <Link to="/login">
+              <img src={MainLogo} alt="메인로고" />
+            </Link>
+          ) : (
+            <a className="text-title text-[16px] normal-case">{title}</a>
+          )}
+        </div>
+
+        <div className="navbar-end mr-5">
+          {/* 북마크 */}
+          {showBookmark && (
+            <button onClick={handleBookmarkClick}>
+              <BookmarkIcon />
+            </button>
+          )}
+
+          {/* 보관함 삭제 버튼 */}
+          {showDelete && !selectionMode && (
+            <Button variant="archive" className=" mr-1" onClick={enterSelectionMode}>
+              Delete
+            </Button>
+          )}
+          {selectionMode && !deleteMode && (
+            <Button variant="archive" onClick={selectAll}>
+              Select all{' '}
+            </Button>
+          )}
+          {selectionMode && deleteMode && (
+            <Button variant="archive" onClick={delectAll}>
+              Delete All
+            </Button>
+          )}
+        </div>
+
+        {/* 사이드바 표시 */}
+        <Sidebar isOpen={sidebarOpen} onClose={toggleSidebar} />
       </div>
-
-      {/* 로고 및 페이지이름 */}
-      <div className="navbar-center">
-        {/* 임시 Link */}
-        {isMain ? (
-          <Link to="/login">
-            <img src={MainLogo} alt="메인로고" />
-          </Link>
-        ) : (
-          <a className="text-title text-[16px] normal-case">{title}</a>
-        )}
-      </div>
-
-      <div className="navbar-end mr-5">
-        {/* 북마크 */}
-        {showBookmark && (
-          <button onClick={handleBookmarkClick}>
-            <BookmarkIcon />
-          </button>
-        )}
-
-        {/* 보관함 삭제 버튼 */}
-        {showDelete && !selectionMode && (
-          <Button variant="archive" className=" mr-1" onClick={enterSelectionMode}>
-            Delete
-          </Button>
-        )}
-        {selectionMode && !deleteMode && (
-          <Button variant="archive" onClick={selectAll}>
-            Select all{' '}
-          </Button>
-        )}
-        {selectionMode && deleteMode && (
-          <Button variant="archive" onClick={delectAll}>
-            Delete All
-          </Button>
-        )}
-      </div>
-
-      {/* 사이드바 표시 */}
-      <Sidebar isOpen={sidebarOpen} onClose={toggleSidebar} />
-    </div>
+      {/* 나가기 확인 모달 */}
+      <ExitModal open={modalOpen} onConfirm={handleConfirm} onCancel={handleCancel} />
+    </>
   )
 }
 
