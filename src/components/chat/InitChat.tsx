f708c5f9061139ec3147e42f55d0c701d8325663
somin0567 <koala0567@gmail.com>
2025-10-07 04:01:00 +0900

    Feat: 첫 채팅 수정

--- src/components/chat/InitChat.tsx
+++ src/components/chat/InitChat.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ChatBubble from './ChatBubble'
+import DescriptionBubble from './DescriptionBubble'
 
-const InitChat: React.FC = () => {
+interface InitChatProps {
+  onReady?: () => void
+}
+
+const InitChat: React.FC<InitChatProps> = ({ onReady }) => {
+  useEffect(() => {
+    // 마운트 완료 시점에 알림
+    onReady?.()
+  }, [onReady])
+
   return (
     <div>
       <ChatBubble
-        message="뭐라고 답변해야할지 고민될 땐, 아래의 추천 문구를 선택해봐!"
+        message="배고파~ 치맥 먹으러 갈래?"
+        isSender={false}
+        avatarUrl="/public/chat/lover.svg"
+        variant="basic"
+        showIcon={true}
+      />
+      <DescriptionBubble
+        word="치맥"
+        pronunciation="chi-maek"
+        descriptionByTab={{
+          Kor: '맥주와 치킨을 같이 즐기는 어쩌구',
+          Eng: 'A Korean slang term for the popular pairing of fried chicken and beer (maekju).',
+        }}
+      />
+      <ChatBubble
+        message="Let’s continue the conversation about what kind of chicken you want to eat!"
         isSender={false}
         variant="second"
       />
