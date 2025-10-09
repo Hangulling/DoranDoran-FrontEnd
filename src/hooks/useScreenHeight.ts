5bd9de3f2f80d2d5aabc23ebec0a80d55c43985b
somin0567 <koala0567@gmail.com>
2025-10-09 19:09:30 +0900

    Feat: 키보드 밀림 확인

---
+++ src/hooks/useScreenHeight.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+
+const useScreenHeight = () => {
+  const [height, setHeight] = useState(window.innerHeight)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setHeight(window.innerHeight)
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  return height
+}
+
+export default useScreenHeight
