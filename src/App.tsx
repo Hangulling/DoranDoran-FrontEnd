5162ffcea94bbb7c27b0b65cffaeb5f688352365
somin0567 <koala0567@gmail.com>
2025-10-10 05:49:23 +0900

    Feat: 키보드 밀림 확인

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { Suspense } from 'react'
 import { Routes } from './router/routes'
 import LoadingSpinner from './components/common/LoadingSpinner'
 import AppLayout from './layouts/AppLayout'
-//import { Toaster } from 'react-hot-toast'
+import { Toaster } from 'react-hot-toast'
 import Div100vh from 'react-div-100vh'
 
 function App() {
   return (
     <Div100vh className="relative mx-auto w-full max-w-md bg-white">
-      {/*<Toaster position="bottom-center" />*/}
+      <Toaster position="bottom-center" />
       <AppLayout>
         <Suspense fallback={<LoadingSpinner />}>
           <Routes />
