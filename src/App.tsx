a77a1fe9b938105087fd76d7c163bc1ee1a1842f
somin0567 <koala0567@gmail.com>
2025-10-13 02:58:33 +0900

    Design: 레이아웃 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
-import { Suspense, useEffect } from 'react'
+import { Suspense } from 'react'
 import { Routes } from './router/routes'
 import LoadingSpinner from './components/common/LoadingSpinner'
 import AppLayout from './layouts/AppLayout'
 import { Toaster } from 'react-hot-toast'
 
 function App() {
-  useEffect(() => {
-    const setAppHeight = () => {
-      const appHeight = window.visualViewport?.height || window.innerHeight
-      document.documentElement.style.setProperty('--app-height', `${appHeight}px`)
-    }
-
-    window.visualViewport?.addEventListener('resize', setAppHeight)
-    setAppHeight()
-    return () => {
-      window.visualViewport?.removeEventListener('resize', setAppHeight)
-    }
-  }, [])
-
   return (
-    <div
-      className="relative mx-auto w-full max-w-md bg-white h-screen flex flex-col"
-      style={{ height: 'var(--app-height, 100vh)' }}
-    >
+    <div className="relative mx-auto w-full max-w-md bg-white h-screen flex flex-col">
       <Toaster position="bottom-center" />
       <AppLayout>
         <Suspense fallback={<LoadingSpinner />}>
