fd764454ac561ca92038a9b53470a844f4e2606d
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-10-16 00:48:53 +0900

    Feat: ExpressionCard 확장 기능 및 DescriptionBubble 연동 추가

--- src/components/archive/ExpressionCard.tsx
+++ src/components/archive/ExpressionCard.tsx
@@ -2,10 +2,13 @@ import check from '../../assets/icon/checkAll.svg'
 import checkOff from '../../assets/icon/disabledCheckAll.svg'
 import speakerOn from '../../assets/icon/activeVolume.svg'
 import speakerOff from '../../assets/icon/defaultVolume.svg'
-// import expandIcon from '../../assets/icon/expandArrow.svg'
+import expandIcon from '../../assets/icon/expandArrow.svg'
+import clsoeExpandIcon from '../../assets/icon/arrowUp.svg'
 import useArchiveStore from '../../stores/useArchiveStore'
 import useTTS from '../../hooks/useTTS'
 import Badge, { type BadgeVariant } from '../common/Badge'
+import DescriptionBubble from '../chat/DescriptionBubble'
+import { useState } from 'react'
 
 interface ExpressionCardProps {
   item: {
@@ -17,6 +20,7 @@ interface ExpressionCardProps {
 
 export default function ExpressionCard({ item }: ExpressionCardProps) {
   const { selectionMode, selectedIds, toggleSelect } = useArchiveStore()
+  const [openCard, setOpenCard] = useState(false)
   const isSelected = selectedIds.has(item.id)
   const { onPlay, playing } = useTTS(item.text)
   const getBadgeVariant = (level: number): BadgeVariant => {
@@ -42,35 +46,54 @@ export default function ExpressionCard({ item }: ExpressionCardProps) {
   }
 
   return (
-    <div className="flex justify-center items-center mb-3" onClick={handleClick}>
-      <div
-        className={`flex flex-col justify-center items-center border ${
-          isSelected ? 'border-green-300 bg-green-50' : 'border-gray-100 bg-white'
-        } w-[335px] h-[71px] rounded-xl`}
-      >
-        <div className="flex w-full justify-between px-4">
-          <Badge variant={getBadgeVariant(item.intimacy)} />
+    <div className="flex flex-col justify-center items-center mb-3" onClick={handleClick}>
+      <div>
+        <div
+          className={`flex flex-col justify-center items-center border ${
+            isSelected ? 'border-green-300 bg-green-50' : 'border-gray-100 bg-white'
+          } w-[335px] min-h-[71px] rounded-xl`}
+        >
+          <div className="flex w-full justify-between px-4 pt-[10px]">
+            <Badge variant={getBadgeVariant(item.intimacy)} />
 
-          {selectionMode && (
-            <img
-              src={isSelected ? check : checkOff}
-              className="w-5 h-5"
-              alt={isSelected ? '선택됨' : '선택 안 됨'}
-            />
-          )}
-        </div>
-
-        <div className="flex justify-between w-full px-4 mt-1">
-          <div className="flex">
-            <img
-              src={playing ? speakerOn : speakerOff}
-              className="w-5 h-5 mr-1"
-              alt={playing ? '읽는 중' : '읽기'}
-              onClick={handleSpeakerClick}
-            />
-            <span className="text-body text-sm text-gray-800 truncate">{item.text}</span>
+            {selectionMode && (
+              <img
+                src={isSelected ? check : checkOff}
+                className="w-5 h-5"
+                alt={isSelected ? '선택됨' : '선택 안 됨'}
+              />
+            )}
           </div>
-          {/* <img src={expandIcon} /> */}
+
+          <div className="flex justify-between w-full px-4 pb-[10px] mt-1">
+            <div className="flex">
+              <img
+                src={playing ? speakerOn : speakerOff}
+                className="w-5 h-5 mr-1"
+                alt={playing ? '읽는 중' : '읽기'}
+                onClick={handleSpeakerClick}
+              />
+              <span className="text-body text-sm text-gray-800 truncate">{item.text}</span>
+            </div>
+            <button onClick={() => setOpenCard(prev => !prev)}>
+              <img src={openCard ? clsoeExpandIcon : expandIcon} />
+            </button>
+          </div>
+
+          {openCard && (
+            <div>
+              <div className="mx-4 my-2 h-px bg-gray-80" />
+              <DescriptionBubble
+                variant="archive"
+                word="치맥"
+                pronunciation="chi-maek"
+                descriptionByTab={{
+                  Kor: '맥주와 치킨을 같이 즐기는 어쩌구',
+                  Eng: 'A Korean slang term for the popular pairing of fried chicken and beer (maekju).',
+                }}
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
