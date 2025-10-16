dfd3902b5e51f30337c0a733b9367b71f675765a
somin0567 <koala0567@gmail.com>
2025-10-16 19:26:21 +0900

    Feat: MSW 비활성화

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,10 @@ import App from './App.tsx'
 import './styles/index.css'
 
 const isDev = import.meta.env.DEV
+const USE_MSW = import.meta.env.VITE_USE_MSW === 'false'
 
 const prepare = async () => {
-  if (isDev) {
+  if (isDev && USE_MSW) {
     const { worker } = await import('./mocks/browser')
     await worker.start()
   }
