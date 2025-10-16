dfa6c60de1d4f51d9c2ce6387c8a155d107d266e
somin0567 <koala0567@gmail.com>
2025-10-16 20:06:57 +0900

    Feat: 엔드포인트 수정

--- src/api/archive.ts
+++ src/api/archive.ts
@@ -1,25 +1,25 @@
-import api from './api'
+import { chatApi } from './api'
 import { BOOKMARK_ENDPOINTS } from './endpoints'
 
 export async function getAllBookmarks() {
-  const res = await api.get(BOOKMARK_ENDPOINTS.LIST_ALL)
+  const res = await chatApi.get(BOOKMARK_ENDPOINTS.LIST_ALL)
   return res.data
 }
 
 export async function getBookmarksByCursor(lastId?: string, size = 20) {
-  const res = await api.get(BOOKMARK_ENDPOINTS.LIST_CURSOR(lastId, size))
+  const res = await chatApi.get(BOOKMARK_ENDPOINTS.LIST_CURSOR(lastId, size))
   return res.data
 }
 
 export async function getBookmarksByBotType(botType: 'friend' | 'honey' | 'coworker' | 'senior') {
-  const res = await api.get(BOOKMARK_ENDPOINTS.LIST_BY_BOT_TYPE(botType))
+  const res = await chatApi.get(BOOKMARK_ENDPOINTS.LIST_BY_BOT_TYPE(botType))
   return res.data
 }
 
 export async function deleteBookmark(bookmarkId: string) {
-  await api.delete(BOOKMARK_ENDPOINTS.DELETE_ONE(bookmarkId))
+  await chatApi.delete(BOOKMARK_ENDPOINTS.DELETE_ONE(bookmarkId))
 }
 
 export async function deleteManyBookmarks(bookmarkIds: string[]) {
-  await api.delete(BOOKMARK_ENDPOINTS.DELETE_MANY, { data: bookmarkIds })
+  await chatApi.delete(BOOKMARK_ENDPOINTS.DELETE_MANY, { data: bookmarkIds })
 }
