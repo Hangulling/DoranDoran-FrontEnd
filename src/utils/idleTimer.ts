b5530aaf727774c9e12c73618d4f9f40fbf669d4
somin0567 <koala0567@gmail.com>
2025-11-04 00:46:14 +0900

    Comment: 주석 처리

--- src/utils/idleTimer.ts
+++ src/utils/idleTimer.ts
@@ -11,7 +11,7 @@ function resetIdleTimer() {
   }
 
   // (디버깅용) 타이머가 (재)설정될 때 로그
-  console.log(`비활성 타이머가 ${IDLE_TIMEOUT_MS / 1000}초로 재설정`)
+  // console.log(`비활성 타이머가 ${IDLE_TIMEOUT_MS / 1000}초로 재설정`)
 
   idleTimer = setTimeout(() => {
     console.warn(`auth:inactive 이벤트를 발생.`)
@@ -29,7 +29,7 @@ export function stopIdleTimer() {
   events.forEach(event => {
     window.removeEventListener(event, resetIdleTimer, { capture: true })
   })
-  console.log('비활성 타이머 중지')
+  //  console.log('비활성 타이머 중지')
 }
 
 export function startIdleTimer() {
