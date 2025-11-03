e6560fd4d2ede65df1c1a9287602c96aa026eee5
김소민 <koala0567@gmail.com>
2025-11-04 01:17:23 +0900

    Chore: 키보드 감지 로직 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { Suspense, useEffect } from 'react'
+import { Suspense } from 'react'
 import { Routes } from './router/routes'
 import LoadingSpinner from './components/common/LoadingSpinner'
 import AppLayout from './layouts/AppLayout'
 import { Toaster } from 'react-hot-toast'
-import { useIsOpenKeyboard } from './hooks/useIsOpenKeyboard'
+// import { useIsOpenKeyboard } from './hooks/useIsOpenKeyboard'
 
 function App() {
-  const { isOpen, viewportHeight } = useIsOpenKeyboard()
+  // const { isOpen, viewportHeight } = useIsOpenKeyboard()
 
-  useEffect(() => {
-    if (isOpen) {
-      document.body.style.height = `${viewportHeight}px`
-    } else {
-      document.body.style.height = '100dvh'
-    }
-  }, [isOpen, viewportHeight])
+  // useEffect(() => {
+  //   if (isOpen) {
+  //     document.body.style.height = `${viewportHeight}px`
+  //   } else {
+  //     document.body.style.height = '100dvh'
+  //   }
+  // }, [isOpen, viewportHeight])
 
   return (
     <div className="h-dvh w-full bg-white">
