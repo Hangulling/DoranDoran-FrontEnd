4f8f73baf18cc67418f81f5a27e7c15f4e46d418
somin0567 <koala0567@gmail.com>
2025-11-03 22:01:45 +0900

    Chore: 사용자 비활성화 타이머 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './styles/index.css'
+import { setupIdleTimer } from './utils/idleTimer.ts'
+import React from 'react'
 
 const isDev = import.meta.env.DEV
-const USE_MSW = import.meta.env.VITE_USE_MSW === 'false'
+const USE_MSW = import.meta.env.VITE_USE_MSW === 'true' // 환경변수에 false 변경
 
 const prepare = async () => {
   if (isDev && USE_MSW) {
@@ -13,14 +15,16 @@ const prepare = async () => {
   }
 }
 
+setupIdleTimer() // 비활성 타이머
+
 prepare().then(() => {
   const container = document.getElementById('root')!
   const root = ReactDOM.createRoot(container)
   root.render(
-    //  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    //  </React.StrictMode>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
   )
 })
