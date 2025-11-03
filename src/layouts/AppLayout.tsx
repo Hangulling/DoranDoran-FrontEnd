7c4e500b42122b826aa51f0d8e8b2deee456639b
김소민 <koala0567@gmail.com>
2025-11-04 01:30:07 +0900

    Chore: 동적 클래스 추가

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -96,6 +96,9 @@ const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
   const closenessMatch = pathname.match(/^\/chat\/(\d+)$/)
   const closenessId = closenessMatch ? closenessMatch[1] : null
 
+  const chatMatch = useMatch('/chat/:id')
+  const isChatPage = Boolean(chatMatch)
+
   // 북마크(채팅/친밀)
   const chatRoomMatch = pathname.match(/^\/(chat|closeness)\/(\d+)$/)
   const chatRoomId = chatRoomMatch ? chatRoomMatch[2] : null
@@ -120,6 +123,12 @@ const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
     title = pageTitles[pathname] || '페이지'
   }
 
+  const mainClasses = [
+    'flex-grow',
+    'min-h-0',
+    isChatPage ? 'overflow-hidden' : 'h-full overflow-y-auto',
+  ].join(' ')
+
   return (
     <div
       className="relative mx-auto flex w-full max-w-md flex-col"
@@ -142,9 +151,7 @@ const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
         </header>
       )}
       {!isPublicPage && <SessionAutoLogout />}
-      <main id="app-scroll" className="flex-grow min-h-0 h-full overflow-y-auto">
-        {children}
-      </main>
+      <main className={mainClasses}>{children}</main>
     </div>
   )
 }
