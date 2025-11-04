3776f77245c227a8aec1732393306cdd84311901
somin0567 <koala0567@gmail.com>
2025-11-04 18:29:57 +0900

    Design: 보관함 문구 수정

--- src/components/archive/EmptyCard.tsx
+++ src/components/archive/EmptyCard.tsx
@@ -4,7 +4,7 @@ export default function EmptyCard() {
   return (
     <div className="flex flex-col items-center justify-center py-10 mt-50">
       <EmptyBookmark />
-      <p className="text-subtitle text-lg text-gray-800 my-2">Saved Expressions!</p>
+      <p className="text-subtitle text-lg text-gray-800 my-2">Save Expressions!</p>
       <p className="text-body text-sm text-gray-600">No saved expressions.</p>
       <p className="text-body text-sm text-gray-600 mb-6">Save useful expressions as you learn.</p>
     </div>
