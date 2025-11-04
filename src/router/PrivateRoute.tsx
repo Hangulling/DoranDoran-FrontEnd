75eeceb44f812e409ec2b560e3fff4aad9165246
somin0567 <koala0567@gmail.com>
2025-11-04 18:09:10 +0900

    Chore: 브라우저 닫기 실행 시 로그아웃

--- src/router/PrivateRoute.tsx
+++ src/router/PrivateRoute.tsx
@@ -7,9 +7,10 @@ interface PrivateRouteProps {
 
 export default function PrivateRoute({ children }: PrivateRouteProps) {
   const location = useLocation()
-  const token = localStorage.getItem('accessToken')
+  const token = sessionStorage.getItem('accessToken')
 
   if (!token) {
+    console.log('No token found, redirecting to login.')
     return <Navigate to="/login" state={{ from: location }} replace />
   }
 
