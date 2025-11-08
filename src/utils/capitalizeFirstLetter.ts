ca1e7047e9c666b51e7c1c5674eb90cae6635490
somin0567 <koala0567@gmail.com>
2025-11-08 15:32:44 +0900

    Chore: 이름 첫글자 대문자로 표시

--- src/utils/capitalizeFirstLetter.ts
+++ src/utils/capitalizeFirstLetter.ts
@@ -3,3 +3,13 @@ export const capitalizeFirstLetter = (str: string) => {
   if (!str) return ''
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
 }
+
+// 이름의 첫글자를 대문자로
+export const capitalizeName = (str: string) => {
+  if (!str) return ''
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ')
+}
