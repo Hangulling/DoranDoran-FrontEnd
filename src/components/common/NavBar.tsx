117d8a8c0f2def8e39bc54e20e4e1e05a2bf9cff
somin0567 <koala0567@gmail.com>
2025-11-08 15:11:56 +0900

    Fix: 경로 오류 수정

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -8,7 +8,7 @@ import useArchiveStore from '../../stores/useArchiveStore'
 import Button from './Button'
 import type { NavBarProps } from '../../types/common'
 import { BOT_TO_ROOM } from '../../types/archive'
-import { useNavBar } from '../../hooks/useNavbar'
+import { useNavBar } from '../../hooks/useNavBar'
 
 const NavBar: React.FC<NavBarProps & { onToggleSidebar?: () => void }> = ({
   title,
