26306d40414d45aedff896b4cddd2fbb7fbb227d
somin0567 <koala0567@gmail.com>
2025-12-05 00:09:15 +0900

    capacitor 설치 및 초기화

---
+++ android/app/src/main/java/com/doranchat/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.doranchat.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.doranchat.app',
+  appName: '도란도란',
+  webDir: 'dist'
+};
+
+export default config;
