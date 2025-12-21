456d008b75542040a71fa8ff1192870353c8039c
catisverycute <ivyseo0312@gmail.com>
2025-12-22 03:19:23 +0900

    회원가입 식별 질문 데이터 추가

---
+++ src/constants/IdentityQuestionData.ts
@@ -0,0 +1,11 @@
+export interface IdentityQuestion {
+  label: string
+  value: string
+}
+export const Identity_Questions: IdentityQuestion[] = [
+  { label: 'What is your favorite season?', value: 'season' },
+  { label: 'What is your favorite sport?', value: 'sport' },
+  { label: 'What is your most valuable item?', value: 'item' },
+  { label: 'What is your hometown?', value: 'hometown' },
+  { label: 'Who is your favorite celebrity?', value: 'celebrity' },
+]
