63fb191bf59bbdde0fdd3701ccffce91025b8f17
catisverycute <ivyseo0312@gmail.com>
2026-01-21 03:34:38 +0900

    iOS 네이티브 Google 로그인 오류 수정

--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,23 @@ import React from 'react'
 import MaintenancePage from './pages/MaintenancePage.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { GoogleOAuthProvider } from '@react-oauth/google'
-import { GOOGLE_CLIENT_ID } from './constants/env'
 import { isNativeApp } from './utils/isNativeApp.ts'
 import { SocialLogin } from '@capgo/capacitor-social-login'
+import { GOOGLE_WEB_CLIENT_ID, GOOGLE_IOS_CLIENT_ID } from './constants/env'
 
 const IS_MAINTENANCE_MODE = import.meta.env.VITE_MAINTENANCE_MODE === 'true'
 
 const urlParams = new URLSearchParams(window.location.search)
 const paramInternal = urlParams.get('internal') === 'true'
 
-if (isNativeApp()) {
+const isNative = isNativeApp()
+
+if (isNative) {
   SocialLogin.initialize({
     google: {
-      webClientId: GOOGLE_CLIENT_ID,
+      webClientId: GOOGLE_WEB_CLIENT_ID,
+      iOSClientId: GOOGLE_IOS_CLIENT_ID,
+      iOSServerClientId: GOOGLE_WEB_CLIENT_ID,
     },
   })
 }
@@ -28,7 +32,7 @@ if (paramInternal) {
 }
 
 const isDev = import.meta.env.DEV
-const USE_MSW = import.meta.env.VITE_USE_MSW === 'true' // 환경변수에 false 변경
+const USE_MSW = import.meta.env.VITE_USE_MSW === 'true'
 
 const prepare = async () => {
   if (isDev && USE_MSW) {
@@ -38,11 +42,15 @@ const prepare = async () => {
 }
 
 const queryClient = new QueryClient()
-console.log('✅ GOOGLE_CLIENT_ID from env:', GOOGLE_CLIENT_ID)
+
+console.log('✅ isNative:', isNative)
+console.log('✅ GOOGLE_WEB_CLIENT_ID:', GOOGLE_WEB_CLIENT_ID)
+console.log('✅ GOOGLE_IOS_CLIENT_ID:', GOOGLE_IOS_CLIENT_ID)
 
 prepare().then(() => {
   const container = document.getElementById('root')!
   const root = ReactDOM.createRoot(container)
+
   root.render(
     <React.StrictMode>
       <BrowserRouter>
@@ -50,8 +58,12 @@ prepare().then(() => {
           <Routes>
             <Route path="*" element={<MaintenancePage />} />
           </Routes>
+        ) : isNative ? (
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
         ) : (
-          <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+          <GoogleOAuthProvider clientId={GOOGLE_WEB_CLIENT_ID}>
             <QueryClientProvider client={queryClient}>
               <App />
             </QueryClientProvider>
