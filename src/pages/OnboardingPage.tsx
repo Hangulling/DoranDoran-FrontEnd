25d6fc4a64c891fca67fb015bb2b3da2fc966f88
somin <koala0567@gmail.com>
2026-01-28 23:13:30 +0900

    하단 여백 수정

--- src/pages/OnboardingPage.tsx
+++ src/pages/OnboardingPage.tsx
@@ -140,8 +140,8 @@ export default function OnboardingPage() {
       </div>
 
       {/* 버튼 */}
-      <div className="fixed inset-x-0 bottom-0 z-10 flex justify-center">
-        <div className="w-full max-w-app md:max-w-tablet lg:max-w-desktop bg-gray-0 shadow-[0_-1px_4px_0_rgba(0,0,0,0.06)] h-18 px-5 pt-[10px] pb-[calc(10px+env(safe-area-inset-bottom))]">
+      <div className="fixed inset-x-0 bottom-0 z-10 flex justify-center pb-[env(safe-area-inset-bottom)]">
+        <div className="w-full max-w-app md:max-w-tablet lg:max-w-desktop bg-gray-0 shadow-[0_-1px_4px_0_rgba(0,0,0,0.06)] h-18 px-5 py-[10px]">
           {isLastPage ? (
             <Button
               type="submit"
