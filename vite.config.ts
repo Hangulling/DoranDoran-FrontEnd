01d4f1a1a4fd68cd1ae4bcdcb880778b7be2b66d
somin <koala0567@gmail.com>
2026-02-26 02:13:09 +0900

    빌드 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -9,27 +9,5 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: id => {
-          if (id.includes('node_modules')) {
-            if (
-              id.includes('react') ||
-              id.includes('react-dom') ||
-              id.includes('react-router')
-            ) {
-              return 'vendor-react'
-            }
-            if (id.includes('@capacitor')) {
-              return 'vendor-capacitor'
-            }
-            if (id.includes('zustand')) {
-              return 'vendor-store'
-            }
-            return 'vendor'
-          }
-        },
-      },
-    },
   },
 })
