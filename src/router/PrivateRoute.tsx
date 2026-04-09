30f0a45c0cf3d13a2c8008932cba47fad11dd9f1
somin <koala0567@gmail.com>
2026-04-09 20:21:40 +0900

    추가 revert

--- src/router/PrivateRoute.tsx
+++ src/router/PrivateRoute.tsx
@@ -1,7 +1,6 @@
 import type { JSX } from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
 import { tokenService } from '../api/tokenService'
-import { useUserStore } from '../stores/useUserStore'
 
 interface PrivateRouteProps {
   children: JSX.Element
@@ -12,21 +11,11 @@ export default function PrivateRoute({ children }: PrivateRouteProps) {
   const access = tokenService.access
   const refresh = tokenService.refresh
 
-  const { isOnboard, isLoaded } = useUserStore() // 온보딩, 데이터 로드 상태 확인
   const hasToken = !!access || !!refresh
 
   if (!hasToken) {
     console.log('No token found, redirecting to login.')
     return <Navigate to="/login" state={{ from: location }} replace />
-  }
-
-  if (!isLoaded) {
-    return <div className="h-screen w-screen bg-white" />
-  }
-
-  // 온보딩 미완료시 온보딩 페이지
-  if (!isOnboard && location.pathname !== '/onboarding') {
-    return <Navigate to="/onboarding" replace />
   }
 
   return children
